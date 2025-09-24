2bb6ea0f6f3a73c2ba48c84617930a399c8a33d5
patryk-szpital <patryk.koc44@gmail.com>
2025-09-24 12:27:41 +0200

    Changed float[] to Vector3

--- BinarySoundOpenAL_Library/MovementPoint.cs
+++ BinarySoundOpenAL_Library/MovementPoint.cs
@@ -8,7 +8,7 @@ namespace BinarySoundOpenAL_Library
 {
     public class MovementPoint
     {
-        public float[] Position { get; set; } // [x, y, z]
+        public Vector3 Position { get; set; } // [x, y, z]
         public float Speed { get; set; }
         public float TimeStart { get; set; }   // czas rozpoczęcia ruchu do punktu
         public float TimeEnd { get; set; }     // czas zakończenia ruchu w punkcie
